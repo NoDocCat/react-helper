6c34d9a0f5290fbd17c27f22359c91ca4c6b3b97
NoDocCat <nodoccat@outlook.com>
2021-01-20 22:54:34 +0800

    build: 打包时排除外部依赖并调整插件
    仅在构建时使用 cleaner 插件, 因为其会导致文档生成器无法监听变动

--- packages/react-helper/rollup.config.js
+++ packages/react-helper/rollup.config.js
@@ -2,11 +2,12 @@ import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import cleaner from "rollup-plugin-cleaner";
 
-const plugins = [typescript(), cleaner({ targets: ["./dist/"] })];
+const plugins = [typescript()];
 
 // 构建模式执行压缩
 if (!process.env.ROLLUP_WATCH) {
   plugins.push(terser());
+  plugins.push(cleaner({ targets: ["./dist/"] }));
 }
 
 export default {
@@ -16,5 +17,6 @@ export default {
     format: "es",
     sourcemap: true,
   },
+  external: ["react"],
   plugins,
 };
