8d206e416f6faa1d2ceaf1a061f39455d1a8c581
NoDocCat <nodoccat@outlook.com>
2021-02-02 15:09:59 +0800

    fix(hooks): 修复 useBool 错误的状态引用

--- project/src/hooks/useBool.ts
+++ project/src/hooks/useBool.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 /**
  * 使用一个布尔状态
@@ -8,9 +8,9 @@ export function useBool(init: boolean): [boolean, (value?: boolean) => void] {
   const [state, setState] = useState<boolean>(init);
 
   // 当传入布尔值时使用此值, 否则取反当前状态
-  const toggle = (value?: boolean) => {
-    setState(typeof value === "boolean" ? value : !state);
-  };
+  const toggle = useCallback((value?: boolean) => {
+    setState(prevState => (typeof value === "boolean" ? value : !prevState));
+  }, []);
 
   return [state, toggle];
 }
