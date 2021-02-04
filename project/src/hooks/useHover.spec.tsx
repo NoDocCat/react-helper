8c5746f92f92f0d1468b7f2b994a20d171abf355
NoDocCat <nodoccat@outlook.com>
2021-02-04 08:49:36 +0800

    test(hooks): 添加 useHover 延时模式的测试

--- project/src/hooks/useHover.spec.tsx
+++ project/src/hooks/useHover.spec.tsx
@@ -1,9 +1,9 @@
 import React, { useRef } from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { useHover } from "./useHover";
 
-const NoDelayUseDemo: React.FC = () => {
+const NoDelay: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
   const isHover = useHover(ref);
 
@@ -14,14 +14,63 @@ const NoDelayUseDemo: React.FC = () => {
   );
 };
 
+const Delay: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isHover = useHover(ref, 5000);
+
+  return (
+    <div ref={ref} data-testid="target">
+      {String(isHover)}
+    </div>
+  );
+};
+
+jest.useFakeTimers();
+
 describe("hooks/useHover", () => {
   it("no delay use", () => {
-    render(<NoDelayUseDemo />);
+    render(<NoDelay />);
 
     const target = screen.queryByTestId("target");
     expect(target).toHaveTextContent("false");
 
     fireEvent.mouseEnter(target as HTMLDivElement);
+    expect(target).toHaveTextContent("true");
+
+    fireEvent.mouseLeave(target as HTMLDivElement);
+    expect(target).toHaveTextContent("false");
+  });
+
+  it("delay use", () => {
+    render(<Delay />);
+
+    const target = screen.queryByTestId("target");
+    expect(target).toHaveTextContent("false");
+
+    // 延时内移开
+    fireEvent.mouseEnter(target as HTMLDivElement);
+    expect(target).toHaveTextContent("false");
+
+    act(() => {
+      jest.advanceTimersByTime(4999);
+    });
+    expect(target).toHaveTextContent("false");
+
+    fireEvent.mouseLeave(target as HTMLDivElement);
+    expect(target).toHaveTextContent("false");
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    expect(target).toHaveTextContent("false");
+
+    // 延时外移开
+    fireEvent.mouseEnter(target as HTMLDivElement);
+    expect(target).toHaveTextContent("false");
+
+    act(() => {
+      jest.advanceTimersByTime(5000);
+    });
     expect(target).toHaveTextContent("true");
 
     fireEvent.mouseLeave(target as HTMLDivElement);
