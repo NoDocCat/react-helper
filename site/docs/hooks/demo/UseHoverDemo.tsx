8565505a19434d60bd2c218b4c0dc0d21a10f84d
NoDocCat <nodoccat@outlook.com>
2021-07-05 07:21:37 +0800

    chore: 暂时从工程中移除文档生成器

--- site/docs/hooks/demo/UseHoverDemo.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from "react";
-import { useHover } from "@ndct/react-helper";
-
-export const UseHoverDemo: React.FC = () => {
-  const ref = useRef<HTMLSpanElement | null>(null);
-  const state = useHover(ref);
-
-  return <span ref={ref}>{String(state)}</span>;
-};
