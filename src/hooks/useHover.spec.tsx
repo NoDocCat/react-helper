504debe5fdba7feb0f473000e83598d60709b09d
NoDocCat <nodoccat@outlook.com>
2021-07-11 21:58:15 +0800

    refactor: 重构 useHover

--- src/hooks/useHover.spec.tsx
+++ src/hooks/useHover.spec.tsx
@@ -1,54 +1,54 @@
 import React, { useRef } from "react";
-import { render, fireEvent, screen, act } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { useHover } from "./useHover";
-
-const NoDelay: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const isHover = useHover(ref);
-
-  return (
-    <div ref={ref} data-testid="target">
-      {String(isHover)}
-    </div>
-  );
-};
-
-const Delay: React.FC = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const isHover = useHover(ref, 5000);
-
-  return (
-    <div ref={ref} data-testid="target">
-      {String(isHover)}
-    </div>
-  );
-};
 
 jest.useFakeTimers();
 
 describe("hooks/useHover", () => {
-  it("no delay use", () => {
-    render(<NoDelay />);
+  test("无延时使用", () => {
+    const View: React.FC = () => {
+      const ref = useRef<HTMLSpanElement>(null);
+      const isHover = useHover(ref);
+
+      return (
+        <span ref={ref} data-testid="target">
+          {String(isHover)}
+        </span>
+      );
+    };
+
+    render(<View />);
 
     const target = screen.queryByTestId("target");
     expect(target).toHaveTextContent("false");
 
-    fireEvent.mouseEnter(target as HTMLDivElement);
+    fireEvent.mouseEnter(target as HTMLElement);
     expect(target).toHaveTextContent("true");
 
-    fireEvent.mouseLeave(target as HTMLDivElement);
+    fireEvent.mouseLeave(target as HTMLElement);
     expect(target).toHaveTextContent("false");
   });
 
-  it("delay use", () => {
-    render(<Delay />);
+  test("延时使用", () => {
+    const View: React.FC = () => {
+      const ref = useRef<HTMLSpanElement>(null);
+      const isHover = useHover(ref, 5000);
+
+      return (
+        <span ref={ref} data-testid="target">
+          {String(isHover)}
+        </span>
+      );
+    };
+
+    render(<View />);
 
     const target = screen.queryByTestId("target");
     expect(target).toHaveTextContent("false");
 
     // 延时内移开
-    fireEvent.mouseEnter(target as HTMLDivElement);
+    fireEvent.mouseEnter(target as HTMLElement);
     expect(target).toHaveTextContent("false");
 
     act(() => {
@@ -56,7 +56,7 @@ describe("hooks/useHover", () => {
     });
     expect(target).toHaveTextContent("false");
 
-    fireEvent.mouseLeave(target as HTMLDivElement);
+    fireEvent.mouseLeave(target as HTMLElement);
     expect(target).toHaveTextContent("false");
 
     act(() => {
@@ -65,7 +65,7 @@ describe("hooks/useHover", () => {
     expect(target).toHaveTextContent("false");
 
     // 延时外移开
-    fireEvent.mouseEnter(target as HTMLDivElement);
+    fireEvent.mouseEnter(target as HTMLElement);
     expect(target).toHaveTextContent("false");
 
     act(() => {
@@ -73,7 +73,7 @@ describe("hooks/useHover", () => {
     });
     expect(target).toHaveTextContent("true");
 
-    fireEvent.mouseLeave(target as HTMLDivElement);
+    fireEvent.mouseLeave(target as HTMLElement);
     expect(target).toHaveTextContent("false");
   });
 });
--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,49 +1,45 @@
-import { RefObject, useCallback, useEffect, useRef, useState } from "react";
+import { RefObject, useCallback, useEffect, useRef } from "react";
+import { useBoolean } from "./useBoolean";
 
 /**
  * 使用元素 hover 状态
- * @param ref 目标元素 ref 引用
- * @param delay 延时防抖
+ * @param ref   目标元素
+ * @param delay 延时时长(毫秒)
  */
-export function useHover(ref: RefObject<HTMLElement | null>, delay?: number): boolean {
-  const [state, setState] = useState<boolean>(false);
-  const timer = useRef<number | null>(null);
-
-  const cleanTimer = () => {
-    if (!timer.current) return;
-    window.clearTimeout(timer.current);
-    timer.current = null;
-  };
+export function useHover(ref: RefObject<HTMLElement>, delay?: number): boolean {
+  const [state, dispatch] = useBoolean(false);
+  const timer = useRef<number>();
 
   const enterListener = useCallback(() => {
-    cleanTimer();
-
     if (!delay) {
-      setState(true);
+      dispatch(true);
       return;
     }
 
     timer.current = window.setTimeout(() => {
-      setState(true);
-      cleanTimer();
+      dispatch(true);
     }, delay);
-  }, [delay]);
+  }, [delay, dispatch]);
 
   const leaveListener = useCallback(() => {
-    cleanTimer();
-    setState(false);
-  }, []);
+    if (delay && timer.current) {
+      window.clearTimeout(timer.current);
+      timer.current = undefined;
+    }
+
+    dispatch(false);
+  }, [delay, dispatch]);
 
   useEffect(() => {
     if (!ref.current) return;
     const target = ref.current;
 
-    target.addEventListener("mouseenter", enterListener, false);
-    target.addEventListener("mouseleave", leaveListener, false);
+    target.addEventListener("mouseenter", enterListener);
+    target.addEventListener("mouseleave", leaveListener);
 
     return () => {
-      target.removeEventListener("mouseenter", enterListener, false);
-      target.removeEventListener("mouseleave", leaveListener, false);
+      target.removeEventListener("mouseenter", enterListener);
+      target.removeEventListener("mouseleave", leaveListener);
     };
   }, [enterListener, leaveListener, ref]);
 
